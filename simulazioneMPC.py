a36a1a2d0ea5681f9e65d597096a5447220d93ed
michela cappello <michela.cappello@mail.polimit.it>
2025-10-21 16:06:08 +0200

    Change Simulazione MPC

--- simulazioneMPC.py
+++ simulazioneMPC.py
@@ -66,7 +66,7 @@ if __name__ == "__main__":
         "Pb_min_eb":30e3,
         "nnarx_mat": 'OnlineLearning\\NNARX_9-9_H3_bs20_Ts300_Ns300_20251020_154518\\net.mat',
         "c_el": np.genfromtxt('OnlineLearning\\20250501_20250501_MGP_PrezziZonali_Nord.csv', delimiter=';', usecols=(2), skip_header=2),
-        "Potenza":np.genfromtxt('ssnet\\Datasets\\DHN_LOAD\\DHN_ground_truth.csv', delimiter=',', usecols=(3,4,5,6), skip_header=28),#potenza all'istante k
+        "Potenza":np.genfromtxt('OnlineLearning\\DHN_ground_truth.csv', delimiter=',', usecols=(3,4,5,6), skip_header=28),#potenza all'istante k
         "Model":'GP',
         "T_C0":4, #istante in cui inizio ad applicare la legge di controllo
         "L_prev": np.eye(10),
@@ -75,16 +75,16 @@ if __name__ == "__main__":
         
     }
     controller = MPC(Param)
-    total_time = Param["total_hours"] * 3600
+    total_time = Param["tot_hours"] * 3600
     time_step = Param["Ts"]
     num_steps = total_time // time_step + 1
 
-    u=np.zeros[(Param["n_inp"],num_steps)]
-    y=np.zeros[(Param["n_out"],num_steps)]
-    y_rnn=np.zeros[(Param["n_out"],num_steps)]
-    y_pred=np.zeros[(num_steps,Param["n_out"])]
-    Pb_pred=np.zeros[(2,num_steps)]
-    s=np.zeros[(Param["n_slack"],num_steps)]
+    u=np.zeros([Param["n_inp"],num_steps])
+    y=np.zeros([Param["n_out"],num_steps])
+    y_rnn=np.zeros([num_steps,Param["n_out"]])
+    y_pred=np.zeros([Param["n_out"],num_steps])
+    Pb_pred=np.zeros([2,num_steps])
+    s=np.zeros([Param["n_slack"],num_steps])
     final_cost = np.zeros([num_steps])
     computation_time = np.zeros([num_steps])
     time_points = np.arange(0, total_time + 1, time_step)[:num_steps]
@@ -93,7 +93,7 @@ if __name__ == "__main__":
         print(k)
         if Param['Model']=='BNN':
         
-            if k>Param["T_C0"]:
+            if k>Param["T_C0"]-1:
             
                 controller.bayesian_correction(k,y,u)
 
@@ -107,7 +107,7 @@ if __name__ == "__main__":
         else:
             s[:, k ] = slack
         y_rnn[ k + 1,:] = y_nnarx[:, 0]
-        y_pred[:, k + 1] = controller.y_prec[:, 0]
+        y_pred[: , k + 1] = controller.y_prec[:, 0]
         Pb_pred[0, k ] = controller.Pb_prec[0, 0]
         Pb_pred[1, k ] = controller.Pb_prec[1, 0]
         
