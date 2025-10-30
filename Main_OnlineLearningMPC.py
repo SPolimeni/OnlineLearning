9acba6bcf6acdf5466653ca2cee20367f8681121
SPolimeni <simone.polimeni@rse-web.it>
2025-10-30 15:08:54 +0100

    Added traceback logs

--- Main_OnlineLearningMPC.py
+++ Main_OnlineLearningMPC.py
@@ -2,12 +2,15 @@ import numpy as np
 import csv
 import argparse
 import time
+import traceback
 from datetime import datetime, timedelta
+from apscheduler.schedulers.background  import BackgroundScheduler
 
 from class_MPC import MPC
 from utils.DERTF_StatesDefinition       import main_status_readings
 from utils.OPCUA.DERTF_SetPointsWriter  import OPCUA_SetPoint
 from utils.DataManagement               import SaveData
+import os
 
 
 def AuxiliaryParameters(Params):
@@ -21,6 +24,13 @@ def AuxiliaryParameters(Params):
     controller.computation_time     = np.zeros([controller.num_steps])
     controller.time_points          = np.arange(0, controller.total_time + 1, controller.time_step)[:controller.num_steps]
 
+def log_exception(msg):
+    """Log exception message to exceptions log file."""
+    LogsFolder = os.path.join(os.getcwd(), 'LogsFolder')
+    os.makedirs(LogsFolder, exist_ok=True)
+    log_file = os.path.join(LogsFolder, 'Exceptions.log')
+    with open(log_file, 'a') as f:
+        f.write(f"{datetime.now()}: {msg}\n")
 
 def ReadStatus():
     if controller.Opts['Debug']:
@@ -38,25 +48,53 @@ def ReadStatus():
         except Exception as e:
             print(f"Error reading status: {e}")
             print("Keeping previous state values.")
+            tb = traceback.extract_tb(e.__traceback__)
+            if tb:
+                last_trace = tb[-1]
+                msg = f"Error occurred in ReadStatus: {e}\nFile: {last_trace.filename}\nLine: {last_trace.lineno}"
+            else:
+                msg = f"Error occurred in ReadStatus: {e}"
+            log_exception(msg)
+            print(msg)
 
 def SetPointsToDERTF(Solved=True):
 
-    k = controller.t_step
-    GetValue = controller.opti.value if Solved else controller.opti.value
+    try: 
 
-    controller.SetPointsWriter.DataMap['Power_HL1']['Value'] = -1e-3 * GetValue(controller.u_out[0, k]) # W to kW
-    controller.SetPointsWriter.DataMap['Power_HL2']['Value'] = -1e-3 * GetValue(controller.u_out[1, k]) # W to kW
-    controller.SetPointsWriter.DataMap['Power_HL3']['Value'] = -1e-3 * GetValue(controller.u_out[2, k]) # W to kW
-    controller.SetPointsWriter.DataMap['Power_HL4']['Value'] = -1e-3 * GetValue(controller.u_out[3, k]) # W to kW
-    controller.SetPointsWriter.DataMap['T_out_GB']['Value']  = GetValue(controller.u_out[4, k])+2
-    controller.SetPointsWriter.DataMap['T_out_EB']['Value']  = GetValue(controller.u_out[5, k])+2
+        k = controller.t_step
+        GetValue = controller.opti.value if Solved else controller.opti.value
+
+        controller.SetPointsWriter.DataMap['Power_HL1']['Value'] = -1e-3 * GetValue(controller.u_out[0, k]) # W to kW
+        controller.SetPointsWriter.DataMap['Power_HL2']['Value'] = -1e-3 * GetValue(controller.u_out[1, k]) # W to kW
+        controller.SetPointsWriter.DataMap['Power_HL3']['Value'] = -1e-3 * GetValue(controller.u_out[2, k]) # W to kW
+        controller.SetPointsWriter.DataMap['Power_HL4']['Value'] = -1e-3 * GetValue(controller.u_out[3, k]) # W to kW
+        controller.SetPointsWriter.DataMap['T_out_GB']['Value']  = GetValue(controller.u_out[4, k])+2
+        controller.SetPointsWriter.DataMap['T_out_EB']['Value']  = GetValue(controller.u_out[5, k])+2
+    except Exception as e:
+        tb = traceback.extract_tb(e.__traceback__)
+        if tb:
+            last_trace = tb[-1]
+            msg = f"Error occurred in SetPointsToDERTF: {e}\nFile: {last_trace.filename}\nLine: {last_trace.lineno}"
+        else:
+            msg = f"Error occurred in SetPointsToDERTF: {e}"
+        log_exception(msg)
+        print(msg)
 
     node_data = []
     for key in controller.SetPointsWriter.DataMap.keys():
         node_data.append(controller.SetPointsWriter.DataMap[key])
 
-    controller.SetPointsWriter.write_node(node_data)
-
+    try:
+        controller.SetPointsWriter.write_node(node_data)
+    except Exception as e:
+        tb = traceback.extract_tb(e.__traceback__)
+        if tb:
+            last_trace = tb[-1]
+            msg = f"Error occurred while writing to OPC UA server: {e}\nFile: {last_trace.filename}\nLine: {last_trace.lineno}"
+        else:
+            msg = f"Error occurred while writing to OPC UA server: {e}"
+        log_exception(msg)
+        print(msg)
 
 def MPC_solve():
 
@@ -75,15 +113,37 @@ def MPC_solve():
         controller.y_out[1,k] = controller.y_dict['T_return']
         controller.y_out[2,k] = controller.y_dict['flow']
 
+    try:
+
+        if controller.Param['Model']=='BNN':
+            if k > controller.Param["T_C0"]-1:
+                controller.bayesian_correction(k,controller.y_out,controller.u_out)
+
+    except Exception as e:
+        tb = traceback.extract_tb(e.__traceback__)
+        if tb:
+            last_trace = tb[-1]
+            msg = f"Error occurred in Bayesian correction: {e}\nFile: {last_trace.filename}\nLine: {last_trace.lineno}"
+        else:
+            msg = f"Error occurred in Bayesian correction: {e}"
+        log_exception(msg)
+        print(msg)
     
+    try:
 
-    if controller.Param['Model']=='BNN':
-        if k > controller.Param["T_C0"]-1:
-            controller.bayesian_correction(k,controller.y_out,controller.u_out)
+        ### Compute the control law every 15 minutes ###
+        controller.u_control, controller.slack, controller.y_nnarx, controller.computation_time[k] = controller.mpc_controller(
+            k, controller.Param["T_C0"], controller.y_out, controller.u_out, controller.y_rnn_out)
 
-    ### Compute the control law every 15 minutes ###
-    controller.u_control, controller.slack, controller.y_nnarx, controller.computation_time[k] = controller.mpc_controller(
-        k, controller.Param["T_C0"], controller.y_out, controller.u_out, controller.y_rnn_out)
+    except Exception as e:
+        tb = traceback.extract_tb(e.__traceback__)
+        if tb:
+            last_trace = tb[-1]
+            msg = f"Error occurred in MPC controller: {e}\nFile: {last_trace.filename}\nLine: {last_trace.lineno}"
+        else:
+            msg = f"Error occurred in MPC controller: {e}"
+        log_exception(msg)
+        print(msg)
 
     controller.u_out[:, k] = controller.u_control
     # print( u[4:6, k])
@@ -97,12 +157,6 @@ def MPC_solve():
     controller.y_pred_out[:, k + 1] = controller.y_prec[:, 0]
     controller.Pb_pred_out[0, k]    = controller.Pb_prec[0, 0]
     controller.Pb_pred_out[1, k]    = controller.Pb_prec[1, 0]
-
-    ### Save true value every 5 minutes
-    # y[0, k + 1] =
-    # y[1, k + 1] = 
-    # y[2, k + 1] = 
-    
     
     gamma_pred_gas  = 0.034
     COP             = 0.8
@@ -115,20 +169,32 @@ def MPC_solve():
     controller.t_step += 1
 
     
-    ### Save Data ###
-    SaveData(controller.y_out, controller.u_out, controller.s_out, controller.Pb_pred_out, controller.final_cost, 
-    controller.computation_time[k], k, controller.Param["Model"], controller.InitialDate)
+    try:
+        ### Save Data ###
+        SaveData(controller.y_out, controller.u_out, controller.s_out, controller.Pb_pred_out, controller.final_cost, 
+        controller.computation_time[k], k, controller.Param["Model"], controller.InitialDate)
 
-    if np.mod(k,controller.Param["T_C0"]-1)==0 or k==0:
-        #send the control law
-        controller.u_out[:, k] = controller.u_control
+        if np.mod(k,controller.Param["T_C0"]-1)==0 or k==0:
+            #send the control law
+            controller.u_out[:, k] = controller.u_control
 
-        # U: 
-        # prime 4 le potenze in W e negative
-        # T mandata Gas Boiler
-        # T mandata Elctric Boiler
-        if k > 0 and not Opts['Debug']:
-            SetPointsToDERTF()
+            # U: 
+            # prime 4 le potenze in W e negative
+            # T mandata Gas Boiler
+            # T mandata Elctric Boiler
+            if k > 0 and not Opts['Debug']:
+                SetPointsToDERTF()
+
+    except Exception as e:
+        tb = traceback.extract_tb(e.__traceback__)
+        if tb:
+            last_trace = tb[-1]
+            msg = f"Error occurred while saving data or sending setpoints: {e}\nFile: {last_trace.filename}\nLine: {last_trace.lineno}"
+        else:
+            msg = f"Error occurred while saving data or sending setpoints: {e}"
+        log_exception(msg)
+        print(msg)
+
 
 
 
